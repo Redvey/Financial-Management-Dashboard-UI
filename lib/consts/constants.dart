3116c4851b01254165f76015765585fba5af384b
redvey <roopam12cse@gmail.com>
2023-10-31 19:15:04 +0530

    #8 Added image at the header

--- lib/consts/constants.dart
+++ lib/consts/constants.dart
@@ -73,7 +73,7 @@ Widget myCustomAppBar(BuildContext context) {
             const Row(
               children: [
                 CircleAvatar(
-                  backgroundImage: NetworkImage('url_to_profile_image'),
+                  backgroundImage: NetworkImage('https://images.unsplash.com/photo-1527980965255-d3b416303d12?auto=format&fit=crop&q=80&w=2960&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
                   radius: 15,
                 ),
                 SizedBox(width: 8),
