9b5a920256b1d4ee1cbaf46059ce19976f153fb5
redvey <roopam12cse@gmail.com>
2023-11-01 18:16:58 +0530

    #9 Making tab's screen responsive

--- lib/responsive/tab_scaffold.dart
+++ lib/responsive/tab_scaffold.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_management_dashboard/consts/constants.dart';
 import 'package:flutter_management_dashboard/util/latest_transaction.dart';
 
+import '../consts/my_drawer.dart';
 import '../util/bar_graph.dart';
 import '../util/savings_graph.dart';
 
@@ -19,45 +20,48 @@ class _TabletScaffoldState extends State<TabletScaffold> {
 
 
         backgroundColor: myDefaultBackground,
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    myCustomAppBar(context),
-                    const Text(
-                      "Overview",
-                      style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
-                    ),
-                    const SizedBox(height: 10),
-                    const OverviewBoxes(),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 3,
-                          child: StatsBarChart(),
-                        ),
-                        const SizedBox(width: 15),
-                        const Expanded(
-                          flex: 1,
-                          child: SavingsGraph(),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    const LatestTransaction(),
-                  ],
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const MyDrawer(),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      myCustomAppBar(context),
+                      const Text(
+                        "Overview",
+                        style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+                      ),
+                      const SizedBox(height: 10),
+                      const OverviewBoxes(),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: StatsBarChart(),
+                          ),
+                          const SizedBox(width: 10),
+                          const Expanded(
+                            flex: 1,
+                            child: SavingsGraph(),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      const LatestTransaction(),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
